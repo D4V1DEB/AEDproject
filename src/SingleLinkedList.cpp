77a081e37328073b7b7081c0b6e0326b5cb1028e
D4V1DEB <davidespinozabarrios@gmail.com>
2025-07-10 11:29:28 -0500

    Add implementation of SingleLinkedList class

---
+++ src/SingleLinkedList.cpp
@@ -0,0 +1,129 @@
+#include "SingleLinkedList.h"
+#include <iostream>
+
+SingleLinkedList::SingleLinkedList() : m_head(nullptr) {
+	m_tail = &m_head;
+}
+
+SingleLinkedList::~SingleLinkedList() {
+	clear();
+}
+
+node** SingleLinkedList::findPosition(int _data) {
+	node** pointer = &m_head;
+	while (*pointer && (*pointer)->m_data < _data) {
+		pointer = &((*pointer)->m_next);
+	}
+	return pointer;
+}
+
+node** SingleLinkedList::findNode(int _data) {
+	node** pointer = &m_head;
+	while (*pointer && (*pointer)->m_data != _data) {
+		pointer = &((*pointer)->m_next);
+	}
+	return pointer;
+}
+
+bool SingleLinkedList::insertAtBegin(int _data) {
+	node* newNode = new Node(_data);
+	newNode->m_next = m_head
+	m_head = newNode;
+	if (m_tail== &m_head) {
+		m_tail = &newNode->m_next;
+	}
+	return true;
+}
+
+bool SingleLinkedList::insertAtEnd(int _data) {
+	node* newNode = new Node(_data);
+	*m_tail = newNode;
+	m_tail = &newNode->m_next;
+	newNode->m_next = nullptr;
+	return true;
+}
+
+bool SingleLinkedList::insertInOrder(int _data) {
+	node** pointer = findPosition(_data);
+	if (*pointer && (*pointer)->m_data == _data) {
+		return false;
+	}
+
+	node* newNode = new node(_data);
+	newNode->m_next = *pointer;
+	*pointer = newNode;
+	if (newNode->m_next == nullptr) {
+		m_tail = &newNode->m_next;
+	}
+	return true;
+}
+
+bool SingleLinkedList::remove(int _data) {
+	node** pointer = findNode(_data):
+	if (*pointer == nullptr) {
+		return false;
+	}
+
+	node* nodeRemove = *pointer;
+	*pointer = nodeRemove->m_next;
+	if (m_tail == &nodeRemove->m_next) {
+		m_tail = pointer;
+	}
+	delete nodeRemove;
+	return true;
+}
+
+bool SingleLinkedList::find(int _data) {
+	node* current = m_head;
+	while (current) {
+		if (current->m_data == _data) {
+			return true;
+		}
+		current = current->m_next;
+	}
+	return false;
+}
+
+node* SingleLinkedList::findNode(int _data) {
+	node* current = m_head;
+	while (current) {
+		if (current->m_data == _data) {
+			return current;
+		}
+		current = current->m_next;
+	}
+	return nullptr;
+}
+
+void SingleLinkedList::print() const {
+	node* current = m_head;
+	while (current) {
+		std::cout <<current->m_data << " ";
+	}
+	std::cout << "Empty" << std::endl;
+}
+
+bool SingleLinkedList::isEmpty() const {
+	return m_head == nullptr;
+}
+
+bool SingleLinkedList::size() const {
+	int count = 0;
+	node* current = m_head;
+	while(current) {
+		const++;
+		current = current->m_next;
+	}
+	return count;
+}
+
+void SingleLinkedList::clear() {
+	node* current = m_head;
+	while (current) {
+		node* temp = current;
+		current = current->m_next;
+		delete temp;
+	}
+	m_head = nullptr;
+	m_tail = &m_head;
+}
